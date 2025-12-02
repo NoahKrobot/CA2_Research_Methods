b6c97eabcf0249772ddf85922752c3a8ae1dfbaf
NoahKrobot <noah@certus-sustavi.hr>
2025-12-02 15:34:46 +0000

    Add nicely displayed feed 1

--- setup.js
+++ setup.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then((data) => {
       var posts = data.posts || [];
-      var currentTime = Math.floor(Date.now() / 1000); 
+      var currentTime = Math.floor(Date.now() / 1000);
 
       posts.forEach((post) => {
         var engagement =
@@ -23,8 +23,8 @@ document.addEventListener("DOMContentLoaded", () => {
         post.score = score;
       });
 
-      document.getElementById("posts-data-box").textContent =
-        JSON.stringify(posts, null, 2);
+      // document.getElementById("posts-data-box").textContent =
+      //   JSON.stringify(posts, null, 2);
 
       const feedEl = document.getElementById("feed");
 
@@ -32,15 +32,43 @@ document.addEventListener("DOMContentLoaded", () => {
         const div = document.createElement("div");
         div.className = "post-item";
 
+        // div.innerHTML = `
+        //   <h3>${post.published_by}</h3>
+        //   <p><strong>Tag:</strong> ${post.tag}</p>
+        //   <p><strong>Clicks:</strong> ${post.number_of_clicks}</p>
+        //   <p><strong>Votes:</strong> ${post.number_of_votes}</p>
+        //   <p><strong>Comments:</strong> ${post.number_of_comments}</p>
+        //   <p><strong>Score:</strong> ${post.score.toFixed(6)}</p>
+        //   <img src="${post.image}" alt="post image" style="width:150px;margin-top:10px;">
+        // `;
+
         div.innerHTML = `
           <h3>${post.published_by}</h3>
-          <p><strong>Tag:</strong> ${post.tag}</p>
-          <p><strong>Clicks:</strong> ${post.number_of_clicks}</p>
-          <p><strong>Votes:</strong> ${post.number_of_votes}</p>
-          <p><strong>Comments:</strong> ${post.number_of_comments}</p>
+          <p><strong></strong> ${post.tag}</p>
+       
           <p><strong>Score:</strong> ${post.score.toFixed(6)}</p>
-          <img src="${post.image}" alt="post image" style="width:150px;margin-top:10px;">
-        `;
+          <img src="${post.image}" alt="post image" style="
+              width: 100%;
+              max-height: 33vh;
+              margin-top: 10px;"
+          >
+          <div
+          style="
+          display: flex;
+          justify-content: space-evenly;"
+          >
+
+
+          <p><i class="fa fa-heart" aria-hidden="true"></i> ${
+            post.number_of_votes
+          }</p>
+          <p><i class="fa fa-comment" aria-hidden="true"></i> ${
+            post.number_of_comments
+          }</p>
+          </div>
+        
+       
+          `;
 
         feedEl.appendChild(div);
       });
